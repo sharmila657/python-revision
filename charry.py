a776b38771c7a5ecd56c4bc0a3efe817e4b2dc22
sharmila657 <sharmilajirel8@gmail.com>
2023-03-05 12:22:23 +0545

    python revision day1

---
+++ charry.py
@@ -0,0 +1,103 @@
+# a= 50
+# b= 3
+# print("The value of", a, "+", 3 ,"is", a+b)
+# print("The value of", a, "-", 3 ,"is", a-b)
+# print("The value of", a, "*", 3 ,"is", a*b)
+# print("The value of", a, "/", 3 ,"is", a/b)
+
+# a = input("Enter your name:")
+# print("my name is", a)
+
+# x=input("enter first number:")
+# y=input("enter second number:")
+# print(int(x)+(y))
+
+# name="sharmi"
+# friend="jirel"
+# anotherfriend="priya"
+# apple='''He said 
+# hi harry
+# hey i am good
+# "i want to eat apple'''
+# print("hello," +  name)
+# print(apple)
+
+# fruit="apple"
+# aplelength=len(fruit)
+# print(aplelength)
+# print(fruit.upper())
+# print(fruit.replace("apple","mango"))
+
+# a= int(input("Enter your age:"))
+# print("Your age is:", a)
+
+# if(a>18):
+
+#     print("You can drive")
+#     print("yes")
+# else:
+#     print("you cannot drive")  
+#     print("no")  
+
+# applePrice= 100
+# budget = 250
+# if (budget - applePrice < 50):
+#     print("Alexa, add 1 kg Apples to the cart.")
+# elif(budget - applePrice > 70):
+#     print("its okay you can buy")
+# else:
+#     print("Alexa, do not add Apples to the cart.")
+# num = 50
+# if (num<0):
+#     print("Number is negative.")
+# elif (num > 0):
+#     if(num <= 10):
+#         print("Number is between 1-10")
+#     elif (num > 10 and num <= 20):
+#         print("number is between 11- 20")
+#     else:
+#         print("Number is greater than 20") 
+# else:
+#     print("number is zero")  
+
+# if else elif
+
+# num = int(input("Enter the value of num:"))
+# if (num < 0):
+#     print("Number is negative.")
+# elif(num == 0):
+#     print("Number is Special.")
+# elif(num == 999):
+#     print("number is special. ")   
+# else:
+#     print("number is positive.")    
+
+# import time
+# timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp = time.strftime('%H')
+# print(timestamp)
+# timestamp =time.strftime('%M')
+# print(timestamp)
+# timestamp = time.strftime('%S')
+# print(timestamp)
+
+# colors = ["Red", "Green","Blue","Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+
+# name = 'shbarmib'
+# for i in name:
+#     print(i)
+#     if(i == "b"):
+#         print("this is something special!")
+
+colors = ["Red","Green","Blue","Yellow"]
+for color in colors:
+   print(color)
+   for i in color:
+       print(i)   
